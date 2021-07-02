747e3d852ca5c24f8dc353337fbc3b3a4410c349
Tabassum Khan <tabassum.kamal.k@gmail.com>
2021-07-02 20:49:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,15 +20,14 @@ app
 blogController(app);
 
 
-//connect to the database and then connect to the server
-// dbURI = "mongodb+srv://tabassum-khan:Tab_A135$$@cluster0.w1zon.mongodb.net/testDB?retryWrites=true&w=majority";
-
-dbURI = "mongodb+srv://tabassum-khan:Tab_A135$$@blogcluster.ayqek.mongodb.net/testDB?retryWrites=true&w=majority";
+/***
+dbURI = URL to your database
+***/
 
 mongoose.connect(dbURI, { useNewUrlParser: true , useUnifiedTopology: true})
 .then(() => app.listen(3000, function() {
     console.log("Server started on port 3000");
-  })
+  });
 ).catch((err) => console.log(err));
 
 
